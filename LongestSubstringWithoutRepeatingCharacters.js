5f448c1c52efcf655797d6cbec3dddf0e9483d48
alangiovannipolardev <alangiovannipolardev@gmail.com>
2022-05-30 11:32:33 -0500

    feat(Array): add longestSubstringWithoutRepeat

--- LongestSubstringWithoutRepeatingCharacters.js
+++ LongestSubstringWithoutRepeatingCharacters.js
@@ -1,5 +1,5 @@
 /*
- Given a string S. the task is to find the length of the longest substring without repeating characters
+Given a string s, find the length of the longest substring without repeating characters.
 
  Examples:
 
@@ -9,8 +9,31 @@
  Input: S = "pwwkew"
  Output: 3
 
+ Input: s = "bbbbb"
+ Output: 1
+
+
 */
 
-const getLongestSubtring = () => {
+const getLongestSubtring = (s) => {
+  let windowStart = 0
+  let windowEnd = 0
+  let longestSubstringLength = Number.MIN_VALUE;
+  let stringSet = new Set();
+
+  while(windowEnd < s.length) {
+    if(!stringSet.has(windowEnd)) {
+      stringSet.add(s[windowEnd]);
+      windowEnd++
+      longestSubstringLength = Math.max(longestSubstringLength,stringSet.size)
+    }else {
+       stringSet.delete(s[windowStart])
+       windowStart++
+    }
+  }
+
+ return longestSubstringLength;
 
 }
+
+console.log(getLongestSubtring("abcabcbb"))
